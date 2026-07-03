2be9f479921191fd0d556f4abc2af80aa4b72a18
louis <sanicisspeedandfast@gmail.com>
2026-07-02 23:24:49 -0400

    added basic setup, baseline resume

--- reviewer.py
+++ reviewer.py
@@ -1,1 +1,17 @@
 from anthropic import Anthropic
+
+client = Anthropic()
+
+with open("resume_evaluation_system_prompt.txt", "read") as file:
+    system_prompt=file.read()
+
+# Create a message request
+message = client.messages.create(
+    model="claude-sonnet-5",  # Chosen model, ideal for efficiency and intelligence
+    max_tokens=1024,
+    temperature=0,
+    system=system_prompt,
+    messages=[
+        {"role": "user", "content": ""}
+    ]
+)
